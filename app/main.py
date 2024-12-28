7ec3fa3547f38e4109dcf874ccf172925ee7b7a5
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2024-12-28 19:37:21 +0800

    Add session management routes and update hello world response

--- app/main.py
+++ app/main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+import os
+from flask import Flask, session
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+@app.route('/set_session')
+def set_session():
+    session['username'] = "John98"
+    # Avoid storing sensitive information like passwords in the session
+    session['user_id'] = 1  # Example of storing a user ID instead
+    return "set session!"
+
+@app.route('/delete_session')
+def delete_session():
+    print(session)
+    session.pop('username', None)
+    print(session)
+    return "Session deleted"
+    
 
 @app.route("/", methods=["GET"])
 def hello_world():
-    return "<p>sdsd, dss! dsd!</p>"
+    return "<p>Hello World!</p>"
+
 
 if __name__ == "__main__":
     app.run()
